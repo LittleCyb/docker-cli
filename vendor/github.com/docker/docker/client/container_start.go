5cd647d1c288ba649e6d0bbdad8a4a621bbf6f8c
Gideon Landeman <gideonlandeman@gmail.com>
2022-05-04 18:01:37 +0200

    Add portConfigWrapper struct to container_start.go
    Signed-off-by: Gideon Landeman <gideonlandeman@gmail.com>

--- vendor/github.com/docker/docker/client/container_start.go
+++ vendor/github.com/docker/docker/client/container_start.go
@@ -5,7 +5,13 @@ import (
 	"net/url"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/go-connections/nat"
 )
+
+type portConfigWrapper struct {
+	ExposedPorts	map[nat.Port]struct{}
+	PortBindings    map[nat.Port][]nat.PortBinding
+}
 
 // ContainerStart sends a request to the docker daemon to start a container.
 func (cli *Client) ContainerStart(ctx context.Context, containerID string, options types.ContainerStartOptions) error {
@@ -16,6 +22,12 @@ func (cli *Client) ContainerStart(ctx context.Context, containerID string, optio
 	if len(options.CheckpointDir) != 0 {
 		query.Set("checkpoint-dir", options.CheckpointDir)
 	}
+
+	/*body := configWrapper{
+		Config:           config,
+		HostConfig:       hostConfig,
+		NetworkingConfig: networkingConfig,
+	}*/
 
 	resp, err := cli.post(ctx, "/containers/"+containerID+"/start", query, nil, nil)
 	ensureReaderClosed(resp)
